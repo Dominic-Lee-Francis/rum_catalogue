c81b737e49928a994cc9ca2ed71c28161245d223
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-12 20:39:26 +0800

    Set up handlebars and removed errors and added bootstrap to handlebars

--- server/server.js
+++ server/server.js
@@ -1,41 +1,48 @@
 // express package
 const express = require('express');
+// handlebars package
 const {engine} = require('express-handlebars');
+// body-parser package
+const bodyParser = require('body-parser');
+// dotenv package
+require('dotenv').config();
+// app
 const app = express();
 // change localhost port here
 const port = process.env.PORT || 8080;
 
-// bcrypt package - for password hashing
-const bcrypt = require('bcrypt');
-
+// POSTGRESQL CONNECTION
 // connects to the user database routes file
 const userRoutes = require('./src/users/routes');
 
-// Engine
-app.engine('hbs', engine({
-    extname: '.hbs',
-    defaultLayout:false,
-    layoutsDir: 'views'
-}));
+// PARSE APPLICATION
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-// set the view engine to handlebars
-app.set('view engine', 'hbs');
-
-// middleware to post in json format
+// STATIC FILES
+// when using handlebars, etc. it will begin at public level.
+// VERY IMPORTANT
 app.use(express.static('public'));
+
+// JSON
 app.use(express.json());
 
-// Router
-app.get('/index', (req, res) => {
-    res.render('index.hbs');
-});
+// TEMPLATING ENGINE
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
+// ROUTER
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
+// DB URL PATH
 // has to after the app.get and this code is to connect to the user database routes file and our user SQL file
 // type this path to see the user database
 app.use('/api/users', userRoutes);
 
-// start the server on the specified port
+
+// START SERVER
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
